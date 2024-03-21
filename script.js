8d80f780438985a4093d7b5ddb1fee10338c86f7
yeonseochae <chaey826@newschool.edu>
2024-03-21 11:03:57 -0400

    update

---
+++ script.js
@@ -0,0 +1,278 @@
+const books = [ {
+    "toyImg":"images/toys/toy7.png",  
+    "name": "Red Fraggle",
+    "collection": "Fraggle Rock Vegetable Vehicle Cars",
+    "year": "1988",
+    "price": "$6.99",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy32.png",  
+    "name": "Mickey Mouse",
+    "collection": "MICKEY'S BIRTHDAY LAND",
+    "year":"1988",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy21.png",  
+    "name": "Caterpillar",
+    "collection": "Eric Carle Finger Puppet",
+    "year": "1996",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy2.png",  
+    "name": "Sheer Khan",
+    "collection": "The Jungle Book Wind Up Toys",
+    "year": "1989",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy8.png",  
+    "name": "Gobo Fraggle",
+    "collection": "Fraggle Rock Vegetable Vehicle Cars",
+    "year": "1988",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy13.png",  
+    "name": "Charlie",
+    "collection": "Peanuts Charlie Brown",
+    "year": "1990",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy10.png",  
+    "name": "Garfield & Scooter",
+    "collection": "Garfield",
+    "year": "1988",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy11.png",  
+    "name": "Baby Fozzie",
+    "collection": "Muppet Babies",
+    "year": "1987",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy4.png",  
+    "name": "Cruella De Vil",
+    "collection": "Disney 101 Dalmatians",
+    "year": "1990",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy15.png",  
+    "name": "Ronald McDonald the Bear",
+    "collection": "Teenie Beanie Babies (Bears) 25th ",
+    "year": "2004",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy14.png",  
+    "name": "Lucy",
+    "collection": "Peanuts Charlie Brown",
+    "year": "1990",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy24.png",  
+    "name": "Miss Piggy",
+    "collection": "Muppet Treasure Island",
+    "year": "1995",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy6.png",  
+    "name": "Boober & Wembley Fraggle",
+    "collection": "Fraggle Rock Vehicle Cars",
+    "year": "1988",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy12.png",  
+    "name": "Jennifer Lauren Loose",
+    "collection": "Cabbage Patch Kids",
+    "year": "1992",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy5.png",  
+    "name": "Baby Gonzo",
+    "collection": "Muppet Babies",
+    "year": "1987",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy28.png",  
+    "name": "Mulan",
+    "collection": "Disney's Mulan",
+    "year": "1998",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy17.png",  
+    "name": "Mama",
+    "collection": "Berenstain Bear",
+    "year": "1987",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy22.png",  
+    "name": "Babs Bunny",
+    "collection": "Tiny Toons Wacky Rollers",
+    "year": "1992",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy19.png",  
+    "name": "Dopey & Sneezy",
+    "collection": "Snow White & 7 Dwarfs",
+    "year": "1993",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy9.png",  
+    "name": "Monkey Fraggle",
+    "collection": "Fraggle Rock Vegetable Vehicle Cars",
+    "year": "1988",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy16.png",  
+    "name": "Twilight Sparkle",
+    "collection": "My Little Pony",
+    "year": "2012",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy.png",  
+    "name": "Mimi Kristina",
+    "collection": "Cabbage Patch Kids",
+    "year": "1994",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy25.png",  
+    "name": "Bambi",
+    "collection": "Bambi",
+    "year": "1988",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy26.png",  
+    "name": "Jumba",
+    "collection": "Lilo & Stitch",
+    "year": "2002",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy27.png",  
+    "name": "Lilo",
+    "collection": "Lilo & Stitch",
+    "year": "2002",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy29.png",  
+    "name": "Charlie",
+    "collection": "Ice Age 4-Continental Drift",
+    "year": "2012",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy23.png",  
+    "name": "Scuttle",
+    "collection": "Little Mermaid",
+    "year": "1997",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy3.png",  
+    "name": "Pongo",
+    "collection": "Disney 101 Dalmatians",
+    "year": "1990",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy31.png",  
+    "name": "Koala",
+    "collection": "Teenie Beanie Babies",
+    "year": "1998",
+    "price": "$",
+    "poster":"Poster",
+},
+{
+    "toyImg":"images/toys/toy30.png",  
+    "name": "Penguin",
+    "collection": "Teenie Beanie Babies",
+    "year": "1998",
+    "price": "$",
+    "poster":"Poster",
+},
+];
+
+
+window.addEventListener('load', ()=>{
+    init();
+})
+
+const init = function(){
+    const bookShelf = document.getElementById("bookshelf");
+
+    books.map(function(book){
+        const bookItem = buildBookItem(book);
+        bookShelf.innerHTML = bookShelf.innerHTML + bookItem;
+    })
+}
+
+
+const buildBookItem = function(book){
+    return `
+    <div class="book-item">
+
+        <div class="toy-grid">
+            <img src=${book.toyImg}>
+            <h2 class="name">${book.name}</h2>
+            <p class="collection-name">${book.collection}</p>
+            
+            <div class="toy-info">
+                <p>${book.year}</p>
+                <p>${book.price}</p>
+            </div>    
+        </div>  
+
+        <div class="poster">
+            <p>${book.poster}</p>
+        </div>
+        
+    </div>`
+}
